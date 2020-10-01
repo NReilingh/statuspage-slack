18206094964ae18baf1d138c412c80e2bc5461a6
Glitch (statuspage-slack) <none>
2020-10-01 03:15:46 +0000

    👶📇 component colors and logging

--- statuspage_colors.js
+++ statuspage_colors.js
@@ -1,4 +1,4 @@
-let indicator_colors = {
+const page_colors = {
   "critical": "#e74c3c",
   "major": "#e67e22",
   "minor": "#f1c40f",
@@ -6,6 +6,18 @@ let indicator_colors = {
   "none": "#333333"
 };
 
-module.exports = function (status) {
-  return indicator_colors[status] || indicator_colors["none"];
+const component_colors = {
+  "operational": "#2FCC66",
+  "degraded_performance": "#F1C40F",
+  "partial_outage": "#E67E22",
+  "major_outage": "#E74C3C",
+  "under_maintenance": "#3498DB"
+};
+
+module.exports.pageStatusColor = function (status) {
+  return page_colors[status] || page_colors["none"];
+};
+
+module.exports.componentStatusColor = function componentStatusColor(status) {
+  return component_colors[status] || page_colors['none'];
 };
